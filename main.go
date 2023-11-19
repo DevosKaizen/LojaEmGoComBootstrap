e4bf294bde5bb2a944459e5b4930f93d157a8434
AndrewDevos <andrew.cottadevos@gmail.com>
2023-11-19 14:26:58 -0300

    antes de conectar ao postgres a tabela dos produtos

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 type Produto struct {
@@ -16,10 +19,23 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 
+	db := conectaCombancoDeDados()
+	defer db.Close()
+	
 	http.HandleFunc("/", index)
 
 	http.ListenAndServe(":8000", nil)
 
+}
+
+	// Cria conexao com banco de dados e testa se tem erro
+func conectaCombancoDeDados() *sql.DB {
+	conexao := "user=postgres dbname=alura_loja password=admin host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -32,6 +48,5 @@ func index(w http.ResponseWriter, r *http.Request) {
 		{"combo de sushi", "delicioso", 50.55, 10},
 	}
 	temp.ExecuteTemplate(w, "Index", produtos)
-	
 
 }
